e64d698cf087e071eaeaa760db72416e0b48e68a
Onefivefournine <onefivefournine@gmail.com>
2017-11-16 02:13:53 +0300

    transaction points

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const RSI_THRESHOLD = 40;
 
 const DATA_URL = `https://poloniex.com/public?command=returnChartData&currencyPair=${CURRENCY_PAIR}&start=${START}&end=${END}&period=${PERIOD}`;
 
-const DB_URL = 'mysql://root:123@localhost:3306/testdb';
+const DB_URL = 'mysql://root@localhost:3306/testdb';
 
 const sequelizeInstance = new Sequelize(DB_URL, { logging: false });
 const chartData = require('./models/chartData.model')(sequelizeInstance);
@@ -21,166 +21,166 @@ const transaction = require('./models/transaction.model')(sequelizeInstance);
 const bbands = require('./models/bbands.model')(sequelizeInstance);
 
 function getBBands(data, total) {
-  return new Promise((resolve, reject) => {
-    talib.execute({
-      name: "BBANDS",
-      startIdx: 0,
-      endIdx: data.length - 1,
-      inReal: data.map(el => el.close),
-      optInTimePeriod: 21,
-      optInNbDevUp: 2,
-      optInNbDevDn: 2,
-      optInMAType: 0
-    }, function(err, calculations) {
-      if (err) reject(err);
-      if (total) {
-        getRsi(data)
-          .then((rsi) => bbands.create({
-            dates: JSON.stringify(rawData.map(el => el.date)),
-            prices: JSON.stringify(rawData.map(el => el.close)),
-            lowerBand: JSON.stringify(calculations.result.outRealLowerBand),
-            middleBand: JSON.stringify(calculations.result.outRealMiddleBand),
-            upperBand: JSON.stringify(calculations.result.outRealUpperBand),
-            rsi: JSON.stringify(rsi.outReal)
-          }))
-      }
-      resolve(calculations.result);
+    return new Promise((resolve, reject) => {
+        talib.execute({
+            name: "BBANDS",
+            startIdx: 0,
+            endIdx: data.length - 1,
+            inReal: data.map(el => el.close),
+            optInTimePeriod: 21,
+            optInNbDevUp: 2,
+            optInNbDevDn: 2,
+            optInMAType: 0
+        }, function(err, calculations) {
+            if (err) reject(err);
+            if (total) {
+                getRsi(data)
+                    .then((rsi) => bbands.create({
+                        dates: JSON.stringify(rawData.map(el => el.date)),
+                        prices: JSON.stringify(rawData.map(el => el.close)),
+                        lowerBand: JSON.stringify(calculations.result.outRealLowerBand),
+                        middleBand: JSON.stringify(calculations.result.outRealMiddleBand),
+                        upperBand: JSON.stringify(calculations.result.outRealUpperBand),
+                        rsi: JSON.stringify(rsi.outReal)
+                    }))
+            }
+            resolve(calculations.result);
+        })
     })
-  })
 }
 
 function getRsi(data) {
-  return new Promise((resolve, reject) => {
-    talib.execute({
-      name: "RSI",
-      startIdx: 0,
-      endIdx: data.length - 1,
-      inReal: data.map(el => el.close),
-      optInTimePeriod: 14,
-    }, function(err, calculations) {
-      if (err) reject(err);
-      resolve(calculations.result)
+    return new Promise((resolve, reject) => {
+        talib.execute({
+            name: "RSI",
+            startIdx: 0,
+            endIdx: data.length - 1,
+            inReal: data.map(el => el.close),
+            optInTimePeriod: 14,
+        }, function(err, calculations) {
+            if (err) reject(err);
+            resolve(calculations.result)
+        })
     })
-  })
 }
 
 let rawData,
-  hasBuy = false,
-  profit = 0,
-  buyPrice,
-  transactions = [];
+    hasBuy = false,
+    profit = 0,
+    buyPrice,
+    transactions = [];
 
 function buy(lastBuffer) {
-  buyPrice = lastBuffer.close * INVEST_QUANTITY;
-  profit -= buyPrice;
+    buyPrice = lastBuffer.close * INVEST_QUANTITY;
+    profit -= buyPrice;
 
-  transactions.push({
-    date: lastBuffer.date,
-    price: lastBuffer.close,
-    investQuantity: INVEST_QUANTITY,
-    currency: CURRENCY_PAIR,
-    profit,
-    type: 'buy'
-  });
-  console.log('BUY');
-  console.log('Overall profit: ' + profit);
-  hasBuy = true
+    transactions.push({
+        date: lastBuffer.date,
+        price: lastBuffer.close,
+        investQuantity: INVEST_QUANTITY,
+        currency: CURRENCY_PAIR,
+        profit,
+        type: 'buy'
+    });
+    console.log('BUY');
+    console.log('Overall profit: ' + profit);
+    hasBuy = true
 }
 
 function sell(lastBuffer) {
-  let diff = lastBuffer.close * INVEST_QUANTITY - buyPrice;
-  profit = diff;
-  transactions.push({
-    date: lastBuffer.date,
-    price: lastBuffer.close,
-    investQuantity: INVEST_QUANTITY,
-    currency: CURRENCY_PAIR,
-    profit,
-    type: 'sell'
-  });
-  console.log('SELL');
-  console.log('Profit from current: ' + diff);
-  console.log('Overall profit: ' + profit);
-  hasBuy = false
+    let diff = lastBuffer.close * INVEST_QUANTITY - buyPrice;
+    profit = diff;
+    transactions.push({
+        date: lastBuffer.date,
+        price: lastBuffer.close,
+        investQuantity: INVEST_QUANTITY,
+        currency: CURRENCY_PAIR,
+        profit,
+        type: 'sell'
+    });
+    console.log('SELL');
+    console.log('Profit from current: ' + diff);
+    console.log('Overall profit: ' + profit);
+    hasBuy = false
 }
 
 function calculate(buffer) {
-  if (buffer.length > 1) {
-    let lowerBand, upperBand;
-    return getBBands(buffer)
-      .then((bbands) => {
-        lowerBand = bbands.outRealLowerBand;
-        upperBand = bbands.outRealUpperBand;
-        return getRsi(buffer);
-      })
-      .then((rsi) => {
-        let lastLowerBand = lowerBand[lowerBand.length - 1],
-          lastUpperBand = upperBand[upperBand.length - 1]
-        lastRsi = rsi.outReal[rsi.outReal.length - 1],
-          lastBuffer = buffer[buffer.length - 1];
+    if (buffer.length > 1) {
+        let lowerBand, upperBand;
+        return getBBands(buffer)
+            .then((bbands) => {
+                lowerBand = bbands.outRealLowerBand;
+                upperBand = bbands.outRealUpperBand;
+                return getRsi(buffer);
+            })
+            .then((rsi) => {
+                let lastLowerBand = lowerBand[lowerBand.length - 1],
+                    lastUpperBand = upperBand[upperBand.length - 1]
+                lastRsi = rsi.outReal[rsi.outReal.length - 1],
+                    lastBuffer = buffer[buffer.length - 1];
 
-        console.log(lastLowerBand, lastBuffer.close, lastUpperBand, lastRsi);
+                console.log(lastLowerBand, lastBuffer.close, lastUpperBand, lastRsi);
 
-        if (
-          lastLowerBand && lastRsi && lastBuffer.close &&
-          (lastBuffer.close > lastUpperBand ||
-            lastRsi < RSI_THRESHOLD) &&
-          hasBuy
-        ) {
-          sell(lastBuffer)
-        } else if (lastUpperBand && lastRsi && lastBuffer.close &&
-          lastBuffer.close < lastLowerBand && !hasBuy) {
-          buy(lastBuffer)
-        }
-      })
-      .catch((err) => {
-        console.error(err)
-      });
-  } else if (buffer.length === 1 && hasBuy) {
-    sell(buffer[buffer.length - 1])
-  } else if (!buffer.length) {
-    return Promise.reject('NO BUFFER LENGTH')
-  }
+                if (
+                    lastLowerBand && lastRsi && lastBuffer.close &&
+                    (lastBuffer.close > lastUpperBand ||
+                        lastRsi < RSI_THRESHOLD) &&
+                    hasBuy
+                ) {
+                    sell(lastBuffer)
+                } else if (lastUpperBand && lastRsi && lastBuffer.close &&
+                    lastBuffer.close < lastLowerBand && !hasBuy) {
+                    buy(lastBuffer)
+                }
+            })
+            .catch((err) => {
+                console.error(err)
+            });
+    } else if (buffer.length === 1 && hasBuy) {
+        sell(buffer[buffer.length - 1])
+    } else if (!buffer.length) {
+        return Promise.reject('NO BUFFER LENGTH')
+    }
 }
 
 Promise.all([
         chartData.sync({ force: true }),
-        transaction.sync(),
+        transaction.sync({ force: true }),
         bbands.sync({ force: true })
     ])
-  .then(() => axios.get(DATA_URL))
-  .then((res) => {
-    rawData = res.data.map((el) => {
-      el.date = moment.unix(el.date);
-      el.period = PERIOD;
-      el.coinPair = CURRENCY_PAIR;
-      el.createdAt = Date.now()
-      return el
-    }).sort((a, b) => {
-      return new Date(a.date) - new Date(b.date);
-    });
-    getBBands(rawData, true)
-      .then(() => getRsi(rawData, true));
+    .then(() => axios.get(DATA_URL))
+    .then((res) => {
+        rawData = res.data.map((el) => {
+            el.date = moment.unix(el.date);
+            el.period = PERIOD;
+            el.coinPair = CURRENCY_PAIR;
+            el.createdAt = Date.now()
+            return el
+        }).sort((a, b) => {
+            return new Date(a.date) - new Date(b.date);
+        });
+        getBBands(rawData, true)
+            .then(() => getRsi(rawData, true));
 
-    chartData.bulkCreate(rawData)
-    return rawData;
-  })
-  .then((result) => {
-    return rawData.reverse().reduce((sum, item, i) => {
-      return sum.then(() => {
-          return calculate(rawData)
-        })
-        .then(() => {
-          rawData.pop();
-        })
-    }, Promise.resolve());
-  })
+        chartData.bulkCreate(rawData)
+        return rawData;
+    })
+    .then((result) => {
+        return rawData.reverse().reduce((sum, item, i) => {
+            return sum.then(() => {
+                    return calculate(rawData)
+                })
+                .then(() => {
+                    rawData.pop();
+                })
+        }, Promise.resolve());
+    })
 
-  .then(() => transaction.bulkCreate(transactions))
-  .then(() => {
-    console.log('End profit is: ' + profit);
-    sequelizeInstance.close()
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+    .then(() => transaction.bulkCreate(transactions))
+    .then(() => {
+        console.log('End profit is: ' + profit);
+        sequelizeInstance.close()
+    })
+    .catch((err) => {
+        console.error(err)
+    })
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const cors = require('cors');
 
-const DB_URL = 'mysql://root:123@localhost:3306/testdb';
+const DB_URL = 'mysql://root@localhost:3306/testdb';
 const sequelizeInstance = new Sequelize(DB_URL);
 const chartData = require('./models/chartData.model')(sequelizeInstance);
 const bbands = require('./models/bbands.model')(sequelizeInstance);
+const transaction = require('./models/transaction.model')(sequelizeInstance);
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,29 +16,38 @@ app.use(cors());
 const APP_PORT = 8888;
 
 Promise.all([
-chartData.sync(),
-bbands.sync()
-])
-  .then(([chartData, bbands]) => {
-    app.listen(APP_PORT, () => {
-      console.log('Api is listening on http://localhost:' + APP_PORT);
-    });
-    let resp = {}
-    app.get('/raw-data', (req, res) => {
-      chartData.findAll({ limit: +req.query.limit || 15, offset: +req.query.offset || 0, order: ['date'] })
-        .then((result) => {
-          res.send(result)
-        })
-        .catch(err => { console.error(err) })
-    });
+        chartData.sync(),
+        transaction.sync(),
+        bbands.sync()
+    ])
+    .then(() => {
+        app.listen(APP_PORT, () => {
+            console.log('Api is listening on http://localhost:' + APP_PORT);
+        });
 
-    app.get('/bbands', (req, res) => {
-      bbands.findAll({ limit: 1 })
-        .then((result) => {
-          res.send(result[0])
-        })
-        .catch(err => { console.error(err) })
-    });
+        app.get('/raw-data', (req, res) => {
+            chartData.findAll({ limit: +req.query.limit || 15, offset: +req.query.offset || 0, order: ['date'] })
+                .then((result) => {
+                    res.send(result)
+                })
+                .catch(err => { console.error(err) })
+        });
 
-  })
-  .catch(err => { console.error(err) })
+        app.get('/bbands', (req, res) => {
+            bbands.findAll({ limit: 1 })
+                .then((result) => {
+                    res.send(result[0])
+                })
+                .catch(err => { console.error(err) })
+        });
+
+        app.get('/transactions', (req, res) => {
+            transaction.findAll()
+                .then((result) => {
+                    res.send(result)
+                })
+                .catch(err => { console.error(err) })
+        });
+
+    })
+    .catch(err => { console.error(err) })
